b70831e9ce552a70ab48a03a609518ab0fd5ac51
Gilberto Ayala <gayala@nearsoft.com>
2019-10-08 17:31:34 -0700

    Fixed tons of spacing problems, divided function into 2 different things and fixed imports

--- src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommender.java
+++ src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommender.java
@@ -12,36 +12,45 @@ import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
 import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
-import java.io.*;
-import java.util.*;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.File;
+import java.io.Reader;
+import java.io.InputStreamReader;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.zip.GZIPInputStream;
 
-public class MovieRecommender {
+class MovieRecommender {
     private String pathToFile;
     private int totalReviews;
-    private int totalProducts;
-    private int totalUsers;
     private UserBasedRecommender recommender;
-    public  HashMap<String, Long> hUserId = new HashMap<>();
-    public HashMap<String, Long> hProductId = new HashMap<>();
-    String [] tmpArray;
-    public MovieRecommender(String path) throws IOException, TasteException {
+    private HashMap<String, Long> hUserId = new HashMap<>();
+    private HashMap<String, Long> hProductId = new HashMap<>();
+    private String[] tmpArray;
+    private String csvFile = "dataset.csv";
 
-
-        loadFile(path);
+    MovieRecommender(String path) throws IOException, TasteException {
+        processFile(path);
+        handleMahout();
     }
 
-    public void loadFile(String path) throws IOException, TasteException {
-
+    private void processFile(String path) throws IOException {
         int flag = 0;
         long userId = 0;
         long productId = 0;
-
-        String pathToFile = path;
-        InputStream fileStream = new FileInputStream(pathToFile);
+        InputStream fileStream = new FileInputStream(path);
         InputStream gzipStream = new GZIPInputStream(fileStream);
-        Reader decoder = new InputStreamReader(gzipStream, "US-ASCII");
-        FileWriter fileWriter = new FileWriter("dataset.csv");
+        Reader decoder = new InputStreamReader(gzipStream, StandardCharsets.US_ASCII);
+        FileWriter fileWriter = new FileWriter(csvFile);
         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
         BufferedReader bufferedReader = new BufferedReader(decoder);
         String content;
@@ -49,80 +58,77 @@ public class MovieRecommender {
         String line = "";
         while ((content = bufferedReader.readLine()) != null) {
 
-
             if (content.startsWith("product/productId:") && flag == 0) {
 
                 String tmpProduct;
                 long tmpProductId;
                 tmpProduct = content.split(" ")[1];
-                if(hProductId.containsKey(tmpProduct)){
+                if (hProductId.containsKey(tmpProduct)) {
                     tmpProductId = hProductId.get(tmpProduct);
 
-                }else{
+                } else {
                     tmpProductId = productId;
-                    hProductId.put(tmpProduct,productId++);
+                    hProductId.put(tmpProduct, productId++);
                 }
-//                hProductId.put(tmpProduct, productId);
-//                productId++;
                 line = tmpProductId + ",";
                 flag = 1;
+
             } else if (content.startsWith("review/userId:") && flag == 1) {
                 String tmpUser = content.split(" ")[1];
                 long tmpUserId;
-                if(hUserId.containsKey(tmpUser)){
+                if (hUserId.containsKey(tmpUser)) {
                     tmpUserId = hUserId.get(tmpUser);
-                }else{
+
+                } else {
                     tmpUserId = userId;
-                    hUserId.put(tmpUser,userId++);
+                    hUserId.put(tmpUser, userId++);
                 }
-//                hUserId.put(tmpUser, userId);
-//                userId++;
                 flag = 2;
                 line = tmpUserId + "," + line;
 
             } else if (content.startsWith("review/score:") && flag == 2) {
-                String tmpScore= content.split(" ")[1];
+                String tmpScore = content.split(" ")[1];
                 totalReviews++;
                 flag = 0;
-                line = line +tmpScore+"\n";
+                line = line + tmpScore + "\n";
                 bufferedWriter.write(line);
             }
         }
+
         bufferedWriter.flush();
+        fileWriter.close();
+    }
 
-
+    private void handleMahout() throws TasteException, IOException {
         tmpArray = new String[hProductId.size()];
-        for(Map.Entry<String, Long> me : hProductId.entrySet()) {
-            tmpArray[me.getValue().intValue()] = me.getKey();
+        for (Map.Entry<String, Long> mEntry : hProductId.entrySet()) {
+            tmpArray[mEntry.getValue().intValue()] = mEntry.getKey();
         }
-        DataModel model = new FileDataModel(new File("dataset.csv"));
+
+        DataModel model = new FileDataModel(new File(csvFile));
         UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
         UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
         this.recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
     }
 
-
-    public int getTotalReviews() {
+    int getTotalReviews() {
         // every time it reads productId, userId, score add 1 to total reviews
         return totalReviews;
     }
 
-    public int getTotalProducts() {
-        totalProducts =hProductId.size();
-        return totalProducts;
+    int getTotalProducts() {
+        return hProductId.size();
     }
 
-    public int getTotalUsers() {
+    int getTotalUsers() {
         // read users and add them to hash table if it is in the hashtable dont and if hes added then update total user
-        totalUsers = hUserId.size();
-        return totalUsers;
+        return hUserId.size();
     }
 
-
-    public List<String> getRecommendationsForUser(String user) throws TasteException {
+    List<String> getRecommendationsForUser(String user) throws TasteException {
         List<String> recommendations = new ArrayList<>();
-        for(RecommendedItem ri : recommender.recommend(hUserId.get(user), 3)){
-            recommendations.add(tmpArray[(int)ri.getItemID()]);
+        for (RecommendedItem recommendedItem : recommender.recommend(hUserId.get(user), 3)) {
+            recommendations.add(tmpArray[(int) recommendedItem.getItemID()]);
         }
         return recommendations;
 
